656750391a9317a21ea39fdb825fa05c6e910180
wucong <1875486458@qq.com>
2020-10-19 23:11:28 +0800

    update ts-cli

---
+++ ts-cli/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+};
